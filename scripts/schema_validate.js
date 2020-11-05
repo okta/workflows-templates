8c24d1bd62179756f7c8c132ef8ac527ade2eac8
Vikram Moharir <64556000+vikrammoharir-okta@users.noreply.github.com>
2020-11-05 14:00:00 -0800

    OKTA-341561:[skip ci] Support running CI script on local env

--- scripts/schema_validate.js
+++ scripts/schema_validate.js
@@ -2,10 +2,10 @@ const Ajv = require('ajv');
 const shell = require('shelljs')
 
 // Fetch the JSON content for schema
+let baseDir;
+let branchName;
+let prWorkFlowJSONFiles;
 const workflowSchema = require('./workflow-schema.json');
-const baseDir = shell.exec('echo ${TRAVIS_BUILD_DIR}').replace(/\n/g, '');
-const branchName = shell.exec('echo ${TRAVIS_BRANCH}');
-const prWorkFlowJSONFiles = shell.exec('git diff --name-only ${TRAVIS_BRANCH}..HEAD -- \'workflows/*/*.json\'').replace(/\n/g, ' ');
 
 // Process PR files with only filter workflows/*/*.json
 function processPRFiles() {
@@ -27,4 +27,20 @@ function processPRFiles() {
    });
 }
 
+function processArgsAndInitializeVals() {
+  if (process.argv.length > 2) {
+    console.log('Running locally');
+    branchName = process.argv.slice(2);
+    console.log(`Testing for git branch  ${branchName}`);
+    baseDir = process.cwd();
+    prWorkFlowJSONFiles = shell.exec(`git diff --name-only ${branchName}..master -- \'workflows/*/*.json\'`).replace(/\n/g, ' ');
+  } else {
+    console.log('Running in Travis-CI config');
+    baseDir = shell.exec(`echo ${TRAVIS_BUILD_DIR}`).replace(/\n/g, '');
+    branchName = shell.exec(`echo ${TRAVIS_BRANCH}`);
+    prWorkFlowJSONFiles = shell.exec(`git diff --name-only ${TRAVIS_BRANCH}..HEAD -- \'workflows/*/*.json\'`).replace(/\n/g, ' ');
+  }
+}
+
+processArgsAndInitializeVals();
 processPRFiles();
