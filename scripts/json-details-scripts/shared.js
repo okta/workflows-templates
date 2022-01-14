7fcc66ce499b8bebee3b02266482fb05a53dfde8
Hossam Mourad <hossam.mourad@okta.com>
2022-01-14 13:25:42 -0500

    check first if flo and tables are not empty

--- scripts/json-details-scripts/shared.js
+++ scripts/json-details-scripts/shared.js
@@ -7,21 +7,20 @@ module.exports = {
   getCountsFromFlopack: function (flopackContent) {
     const counts = {};
 
-    if (Object.keys(flopackContent.data.flos).length) {
+    if (flopackContent.data.flos && Object.keys(flopackContent.data.flos).length) {
       counts.flowCount = Object.keys(flopackContent.data.flos).length;
+      Object.values(flopackContent.data.flos).forEach((flo) => {
+        if (flo.data.display.isCallable && !flo.data.scheduled) {
+          counts.helperFlowsCount ? counts.helperFlowsCount++ : (counts.helperFlowsCount = 1);
+        } else {
+          counts.mainFlowsCount ? counts.mainFlowsCount++ : (counts.mainFlowsCount = 1);
+        }
+      });
     }
 
-    if (Object.keys(flopackContent.data.tables).length) {
+    if (flopackContent.data.tables && Object.keys(flopackContent.data.tables).length) {
       counts.stashCount = Object.keys(flopackContent.data.tables).length;
     }
-
-    Object.values(flopackContent.data.flos).forEach((flo) => {
-      if (flo.data.display.isCallable && !flo.data.scheduled) {
-        counts.helperFlowsCount ? counts.helperFlowsCount++ : (counts.helperFlowsCount = 1);
-      } else {
-        counts.mainFlowsCount ? counts.mainFlowsCount++ : (counts.mainFlowsCount = 1);
-      }
-    });
 
     return counts;
   }
