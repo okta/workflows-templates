3b810cd5f50931c7366e7512dea3aabc20de54ca
Hossam Mourad <hossam.mourad@okta.com>
2022-02-23 15:10:09 -0500

    modify the details scripts to add flows data to the workflow json files

--- scripts/json-details-scripts/details_modifier.js
+++ scripts/json-details-scripts/details_modifier.js
@@ -1,28 +1,23 @@
 const fs = require("fs");
-const { getCountsFromFlopack } = require("./shared");
+const { getDetailsFromFlopack } = require("./shared");
 
 const workflowsDir = `${process.cwd()}/workflows`;
 const workflows = fs.readdirSync(workflowsDir);
 
 workflows.forEach((workflowName) => {
+  if (workflowName === ".DS_Store") return;
   fs.readFile(`${workflowsDir}/${workflowName}/workflow.flopack`, (_, data) => {
     const flopackContent = JSON.parse(data.toString());
-    const counts = getCountsFromFlopack(flopackContent);
-    writeToJSONFile(workflowName, counts);
+    const details = getDetailsFromFlopack(flopackContent);
+    writeToJSONFile(workflowName, details);
   });
 });
 
-/**
- * Writes the calculated counts to the JSON file representing the flopack
- * @param workflowName(string): the workflow folder name
- * @param newCountsVal(object): an object containing counts for each field
- * @returns null
- */
-function writeToJSONFile(workflowName, newCountsVal) {
+function writeToJSONFile(workflowName, details) {
   const jsonFilePath = `${workflowsDir}/${workflowName}/workflow.json`;
   fs.readFile(jsonFilePath, (_, data) => {
     const content = JSON.parse(data.toString());
-    content.details = newCountsVal;
-    fs.writeFileSync(jsonFilePath, JSON.stringify(content, null, 2));
+    content.details = details;
+    fs.writeFileSync(jsonFilePath, JSON.stringify(content, null, 2) + '\n');
   });
 }
--- scripts/json-details-scripts/details_validator.js
+++ scripts/json-details-scripts/details_validator.js
@@ -1,41 +1,79 @@
 const fs = require("fs");
-const { getCountsFromFlopack } = require("./shared");
+const { getDetailsFromFlopack } = require("./shared");
+
+const modifierScriptMsg = `You can run the "details_modifier" script to fill in valid data for ALL the workflow json files in the repo.`;
 
 const workflowsDir = `${process.cwd()}/workflows`;
 const workflows = fs.readdirSync(workflowsDir);
 
 workflows.forEach((workflowName) => {
+  if (workflowName === ".DS_Store") return;
+
   fs.readFile(`${workflowsDir}/${workflowName}/workflow.flopack`, (_, data) => {
     const flopackContent = JSON.parse(data.toString());
     const jsonContent = JSON.parse(
       fs.readFileSync(`${workflowsDir}/${workflowName}/workflow.json`).toString()
     );
+    const detailsInJSON = jsonContent.details;
+    const detailsFromFlopack = getDetailsFromFlopack(flopackContent);
 
-    if (Object.keys(jsonContent.details).length > 0) {
-      const countsInJSON = jsonContent.details;
-      const countsInFlopack = getCountsFromFlopack(flopackContent);
-      Object.keys(countsInFlopack).forEach((key) => {
-        if (countsInFlopack[key] > 0 && !countsInJSON[key]) {
-          throw new Error(
-            `The "details" field on "${workflowName}/workflow.json" is missing the "${key}" field.`
-          );
-        }
-
-        if (countsInFlopack[key] !== countsInJSON[key]) {
-          throw new Error(
-            `The "details.${key}" field at ${workflowName}/workflow.json is incorrect.`
-          );
-        }
-
-        if (jsonContent.details.useCases) {
-          validateUseCases(jsonContent.details.useCases, workflowName);
-        }
-      });
-    }
+    validateCounts(workflowName, detailsFromFlopack, detailsInJSON);
+    validateFlows(workflowName, detailsFromFlopack, detailsInJSON);
+    validateUseCases(jsonContent.details.useCases, workflowName);
   });
 });
 
+function validateCounts(workflowName, detailsFromFlopack, detailsInJSON) {
+  const countFields = ["flowCount", "mainFlowsCount", "helperFlowsCount", "stashCount"];
+  countFields.forEach((field) => {
+    if (detailsInJSON[field] === 0) {
+      throw new Error(
+        `The "${field}" field at "${workflowName}/workflow.json" can't be a zero. Zero-value fields should be deleted. ${modifierScriptMsg}`
+      );
+    }
+
+    if (detailsFromFlopack[field] > 0 && !detailsInJSON[field]) {
+      throw new Error(
+        `The "details" field at "${workflowName}/workflow.json" is missing the "${field}" field. ${modifierScriptMsg}`
+      );
+    }
+
+    if (detailsFromFlopack[field] !== detailsInJSON[field]) {
+      console.log(workflowName, detailsFromFlopack[field], detailsInJSON[field]);
+      throw new Error(
+        `The "details.${field}" field at ${workflowName}/workflow.json is incorrect. ${modifierScriptMsg}`
+      );
+    }
+  });
+}
+
+function validateFlows(workflowName, detailsFromFlopack, detailsInJSON) {
+  if (!detailsFromFlopack.flows && !detailsInJSON.flows) return;
+
+  if (detailsFromFlopack.flows) {
+    if (!Array.isArray(detailsInJSON.flows)) {
+      throw new Error(
+        `A "details.flows" field (of type array) should exist at "${workflowName}/workflow.json". ${modifierScriptMsg}`
+      );
+    }
+
+    if (detailsInJSON.flows.length === 0) {
+      throw new Error(
+        `The "details.flows" field at ${workflowName}/workflow.json can't be an empty array. ${modifierScriptMsg}`
+      );
+    }
+
+    if (JSON.stringify(detailsFromFlopack.flows) !== JSON.stringify(detailsInJSON.flows)) {
+      throw new Error(
+        `The flows defined at the "${workflowName}/workflow.json -> details.flows" field don't match what's inside the ".flopack" file of that template. ${modifierScriptMsg}`
+      );
+    }
+  }
+}
+
 function validateUseCases(floUseCases, workflowName) {
+  if (!floUseCases) return;
+
   const useCases = JSON.parse(
     fs.readFileSync(`${process.cwd()}/useCases.json`).toString()
   ).useCases.map((s) => s.name);
