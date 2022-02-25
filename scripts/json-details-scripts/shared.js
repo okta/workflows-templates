e19b640538bb816419a1965a311b7407874f4b88
Hossam Mourad <hossam.mourad@okta.com>
2022-02-25 13:55:05 -0500

    change screenshoturl from fake url to an empty string

--- scripts/json-details-scripts/shared.js
+++ scripts/json-details-scripts/shared.js
@@ -12,7 +12,7 @@ module.exports = {
           id: flo.id,
           name: flo.name,
           type: isHelperFlow ? "HELPER" : "MAIN",
-          screenshotURL: "https://via.placeholder.com/1000x500" // TODO: replace with the actual S3 URL
+          screenshotURL: "" // TODO: replace with the actual S3 URL
         });
         if (isHelperFlow) {
           details.helperFlowsCount ? details.helperFlowsCount++ : (details.helperFlowsCount = 1);
