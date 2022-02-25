371fa2e32a30fb6ececaf8b9d7010c109ff4ccfb
Hossam Mourad <97123127+hossammourad-okta@users.noreply.github.com>
2022-02-25 14:48:25 -0500

    OKTA-473084: modify the details scripts to add flows data to the workflow json files (#141)
    * modify the details scripts to add flows data to the workflow json files
    
    * change screenshoturl from fake url to an empty string
    
    * check for workflow folders before proceeding instead of hardcoding excluded paths
    
    * modify workflow.json files using the details_modifier script
    
    * address comment

--- scripts/json-details-scripts/shared.js
+++ scripts/json-details-scripts/shared.js
@@ -1,28 +1,31 @@
-/**
- * Get the 4 counts we need from the .flopack file
- * @param {object} flopackContent: the content of the .flopack file
- * @returns {flowCount?: number, mainFlowsCount?: number, helperFlowsCount?: number, stashCount?: number}
- */
 module.exports = {
-  getCountsFromFlopack: function (flopackContent) {
+  getDetailsFromFlopack: function (flopackContent) {
     const { flos, tables } = flopackContent.data;
-    const counts = {};
+    const details = {};
 
     if (flos && Object.keys(flos).length) {
-      counts.flowCount = Object.keys(flos).length;
+      details.flows = [];
+      details.flowCount = Object.keys(flos).length;
       Object.values(flos).forEach((flo) => {
-        if (flo.data.display.isCallable && !flo.data.scheduled) {
-          counts.helperFlowsCount ? counts.helperFlowsCount++ : (counts.helperFlowsCount = 1);
+        const isHelperFlow = flo.data.display.isCallable && !flo.data.scheduled;
+        details.flows.push({
+          id: flo.id,
+          name: flo.name,
+          type: isHelperFlow ? "HELPER" : "MAIN",
+          screenshotURL: "" // TODO: replace with the actual S3 URL
+        });
+        if (isHelperFlow) {
+          details.helperFlowsCount ? details.helperFlowsCount++ : (details.helperFlowsCount = 1);
         } else {
-          counts.mainFlowsCount ? counts.mainFlowsCount++ : (counts.mainFlowsCount = 1);
+          details.mainFlowsCount ? details.mainFlowsCount++ : (details.mainFlowsCount = 1);
         }
       });
     }
 
     if (tables && Object.keys(tables).length) {
-      counts.stashCount = Object.keys(tables).length;
+      details.stashCount = Object.keys(tables).length;
     }
 
-    return counts;
+    return details;
   }
 };
