4b040e6cc70c6815759d9ae9dc9d9f5e51a40975
Hossam Mourad <hossam.mourad@okta.com>
2022-03-21 10:23:13 -0400

    test 2

--- scripts/fetch_all_flos_screenshots.js
+++ scripts/fetch_all_flos_screenshots.js
@@ -8,7 +8,6 @@ const fs = require("fs");
 const path = require("path");
 
 (function () {
-  const destination = path.resolve("./all-flos-screenshots");
   const files = glob.readdirSync('./workflows/**/flo-screenshot-*.*');
 
   if (!files || files.length === 0) {
@@ -16,16 +15,18 @@ const path = require("path");
     return;
   };
 
-  if (fs.existsSync(destination)) {
+  const destination = "./all-flos-screenshots";
+
+  if (fs.existsSync(path.resolve(destination))) {
     fs.rmSync(destination, { force: true, recursive: true });
   }
-  fs.mkdirSync(destination);
+  fs.mkdirSync(path.resolve(destination));
 
   files.forEach(file => {
     const fileName = file.split("/").slice(-1)[0];
     fs.copyFileSync(
       path.resolve(file),
-      path.resolve(`${destination}/${fileName}`),
+      path.resolve(`${path.resolve(destination)}/${fileName}`),
       fs.constants.COPYFILE_FICLONE
     );
   });
