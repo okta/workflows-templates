3cb3614b6370f5318ea6b79400266c0aac6f3d31
Hossam Mourad <97123127+hossammourad-okta@users.noreply.github.com>
2022-04-01 12:16:00 -0400

    OKTA-480004: update validation scripts to accommodate for FLOs screenshots (#146)
    * WIP
    
    * generate screenshots using a hash for the file name
    
    * populate the templates metadata with urls for the screenshots
    
    * remove screenshot field from validation
    
    * fix specs
    
    * refactor
    
    * add a validation for the screenshot format
    
    * fix broken screenshots
    
    * remove cursor from screenshots
    
    * remove mappings from screenshots
    
    * add missing screenshot for the new_user_registration template
    
    * replace child with helper
    
    * trigger ci/cd
    
    * print logs for debugging
    
    * rename folder back to use child (tmp)
    
    * template name replace child with helper
    
    * flopack version from 1.3.0 to 1.2.0
    
    * regenerate screenshots
    
    * improve pr files detection in travis script
    
    * break CI to test scripts
    
    * debug change from HEAD to master
    
    * more debugging
    
    * test ci failing
    
    * revert
    
    * use if directory condition instead of git diff
    
    * add comment for clarification
    
    * trigger a ci failure
    
    * trigger another ci failure
    
    * revert
    
    * trigger another ci failure for testing
    
    * revert failure
    
    * fix flow unavailable issue in screenshots
    
    * improve error messages
    
    * merge instead of replace fields in the details modifier script
    
    * rename variable
    
    * specs for the new validate screenshots function
    
    * delete mocks, use inline definitions instead
    
    * use lodash clone instead of jsin parse json stringify

--- scripts/json-details-scripts/details_scripts.spec.js
+++ scripts/json-details-scripts/details_scripts.spec.js
@@ -1,41 +1,47 @@
-const {
-  flopackWithNoFlos,
-  flopackWithMainFlosOnly,
-  flopackWithMainAndHelperFlos,
-  flopackWithTablesOnly,
-  flopackWithHelperFlosOnly,
-  flopackWithFlosAndTables,
-  matchingFlopackAndJSON,
-  jsonWithZeroValueCount,
-  missingFieldInJSON,
-  extraFieldsInJSON,
-  misMatchingCounts,
-  matchingFlosData
-} = require("./mocks.js");
+const fs = require("fs");
+
 const {
   getDetailsFromFlopack,
   validateCounts,
-  modifierScriptMsg,
   validateFlos,
-  validFloField,
-  validateUseCases
+  validateUseCases,
+  validateScreenshots
 } = require("./utils.js");
 
 const workflowName = "workflow_template_name";
 
 describe("getCountsFromFlopack", () => {
   test("no flos nor tables", () => {
-    const result = getDetailsFromFlopack(flopackWithNoFlos);
+    const result = getDetailsFromFlopack({ data: {} });
     expect(result).toEqual({});
   });
 
   test("only tables, no flos", () => {
-    const result = getDetailsFromFlopack(flopackWithTablesOnly);
+    const result = getDetailsFromFlopack({
+      data: {
+        tables: ["whatever", "structure-doesn't-matter-here"]
+      }
+    });
     expect(result).toEqual({ stashCount: 2 });
   });
 
   test("1 main flo", () => {
-    const result = getDetailsFromFlopack(flopackWithMainFlosOnly);
+    const result = getDetailsFromFlopack({
+      data: {
+        flos: [
+          {
+            id: "0787a0a1-f2a0-4939-99ba-a07e2273be47",
+            name: "flo 1",
+            data: {
+              display: {
+                isCallable: false
+              },
+              scheduled: false
+            }
+          }
+        ]
+      }
+    });
     expect(result).toEqual({
       flos: [
         {
@@ -50,7 +56,32 @@ describe("getCountsFromFlopack", () => {
   });
 
   test("only helper flows", () => {
-    const result = getDetailsFromFlopack(flopackWithHelperFlosOnly);
+    const result = getDetailsFromFlopack({
+      data: {
+        flos: [
+          {
+            id: "825ca604-c2ac-4e44-a5b7-34f4fd161668",
+            name: "flo 1",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          },
+          {
+            id: "295257de-b4ca-485e-8f12-f93531697664",
+            name: "flo 2",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          }
+        ]
+      }
+    });
     expect(result).toEqual({
       flos: [
         {
@@ -70,7 +101,42 @@ describe("getCountsFromFlopack", () => {
   });
 
   test("main and helper flos", () => {
-    const result = getDetailsFromFlopack(flopackWithMainAndHelperFlos);
+    const result = getDetailsFromFlopack({
+      data: {
+        flos: [
+          {
+            id: "0787a0a1-f2a0-4939-99ba-a07e2273be47",
+            name: "flo 1",
+            data: {
+              display: {
+                isCallable: false
+              },
+              scheduled: true
+            }
+          },
+          {
+            id: "09536e48-f66b-49dd-989c-36c371c9b1ff",
+            name: "flo 2",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          },
+          {
+            id: "ef777a85-4aef-4c98-8ccd-edabbfdd9960",
+            name: "flo 3",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          }
+        ]
+      }
+    });
     expect(result).toEqual({
       flos: [
         {
@@ -96,7 +162,43 @@ describe("getCountsFromFlopack", () => {
   });
 
   test("flos and tables", () => {
-    const result = getDetailsFromFlopack(flopackWithFlosAndTables);
+    const result = getDetailsFromFlopack({
+      data: {
+        flos: [
+          {
+            id: "0787a0a1-f2a0-4939-99ba-a07e2273be47",
+            name: "flo 1",
+            data: {
+              display: {
+                isCallable: false
+              },
+              scheduled: true
+            }
+          },
+          {
+            id: "09536e48-f66b-49dd-989c-36c371c9b1ff",
+            name: "flo 2",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          },
+          {
+            id: "ef777a85-4aef-4c98-8ccd-edabbfdd9960",
+            name: "flo 3",
+            data: {
+              display: {
+                isCallable: true
+              },
+              scheduled: false
+            }
+          }
+        ],
+        tables: ["whatever", "structure-doesn't-matter-here"]
+      }
+    });
     expect(result).toEqual({
       flos: [
         {
@@ -125,7 +227,32 @@ describe("getCountsFromFlopack", () => {
 
 describe("validateCounts", () => {
   it("passes if counts are matching between flopack and json", () => {
-    matchingFlopackAndJSON.forEach((i) => {
+    [
+      {
+        detailsFromFlopack: {
+          flowCount: 10,
+          mainFlowsCount: 2,
+          helperFlowsCount: 8,
+          stashCount: 1
+        },
+        detailsInJSON: {
+          flowCount: 10,
+          mainFlowsCount: 2,
+          helperFlowsCount: 8,
+          stashCount: 1
+        }
+      },
+      {
+        detailsFromFlopack: {
+          flowCount: 1,
+          mainFlowsCount: 1
+        },
+        detailsInJSON: {
+          flowCount: 1,
+          mainFlowsCount: 1
+        }
+      }
+    ].forEach((i) => {
       expect(() =>
         validateCounts(workflowName, i.detailsFromFlopack, i.detailsInJSON)
       ).not.toThrowError();
@@ -133,45 +260,61 @@ describe("validateCounts", () => {
   });
 
   it("throws an error if there is a zero-value for any of the counts fields", () => {
-    expect(() => validateCounts(workflowName, {}, jsonWithZeroValueCount)).toThrowError(
-      `The "flowCount" field at "${workflowName}/workflow.json" can't be a zero. Zero-value fields should be deleted. ${modifierScriptMsg}`
-    );
+    expect(() => validateCounts(workflowName, {}, { flowCount: 0 })).toThrowError();
   });
 
   it("throws an error if there is a missing field in json but present in flopack", () => {
     expect(() =>
       validateCounts(
         workflowName,
-        missingFieldInJSON.detailsFromFlopack,
-        missingFieldInJSON.detailsInJSON
+        {
+          flowCount: 10,
+          mainFlowsCount: 2,
+          helperFlowsCount: 8,
+          stashCount: 1
+        },
+        {
+          flowCount: 10,
+          mainFlowsCount: 2,
+          helperFlowsCount: 8
+        }
       )
-    ).toThrowError(
-      `The "details" field at "${workflowName}/workflow.json" is missing the "stashCount" field. ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 
   it("throws an error if there is an extra field in the json file that should not be there", () => {
     expect(() =>
       validateCounts(
         workflowName,
-        extraFieldsInJSON.detailsFromFlopack,
-        extraFieldsInJSON.detailsInJSON
+        {
+          flowCount: 1,
+          mainFlowsCount: 1
+        },
+        {
+          flowCount: 1,
+          mainFlowsCount: 1,
+          helperFlowsCount: 1
+        }
       )
-    ).toThrowError(
-      `The "helperFlowsCount" at "${workflowName}/workflow.json}" should not exist, it's not matching what's in the ".flopack" file. ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 
   it("throws an error if the counts are not matching between the flopack and json files", () => {
     expect(() =>
       validateCounts(
         workflowName,
-        misMatchingCounts.detailsFromFlopack,
-        misMatchingCounts.detailsInJSON
+        {
+          flowCount: 2,
+          mainFlowsCount: 1,
+          helperFlowsCount: 1
+        },
+        {
+          flowCount: 3,
+          mainFlowsCount: 1,
+          helperFlowsCount: 1
+        }
       )
-    ).toThrowError(
-      `The "details.flowCount" field at ${workflowName}/workflow.json is incorrect. ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 });
 
@@ -182,47 +325,51 @@ describe("validateFlos", () => {
   });
 
   it("throws an error if the details.flos field doesn't exist or of wrong type", () => {
-    expect(() => validateFlos(workflowName, { flos: [{ id: "a-1" }] }, {})).toThrowError(
-      `A "details.flos" field (of type array) should exist at "${workflowName}/workflow.json". ${modifierScriptMsg}`
-    );
+    expect(() => validateFlos(workflowName, { flos: [{ id: "a-1" }] }, {})).toThrowError();
 
     expect(() =>
       validateFlos(workflowName, { flos: [{ id: "a-1" }] }, { flos: "wrong-type" })
-    ).toThrowError(
-      `A "details.flos" field (of type array) should exist at "${workflowName}/workflow.json". ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 
   it("throws an error if the flos field is an empty array", () => {
-    expect(() => validateFlos(workflowName, { flos: [{ id: "a-1" }] }, { flos: [] })).toThrowError(
-      `The "details.flos" field at ${workflowName}/workflow.json can't be an empty array. ${modifierScriptMsg}`
-    );
+    expect(() => validateFlos(workflowName, { flos: [{ id: "a-1" }] }, { flos: [] })).toThrowError();
   });
 
   it("throws an error if the flos are not matching between the flopack and json files", () => {
     expect(() =>
       validateFlos(workflowName, { flos: [{ id: "a-1" }] }, { flos: [{ id: "b-2" }] })
-    ).toThrowError(
-      `The flos defined at the "${workflowName}/workflow.json -> details.flos" field don't match what's inside the ".flopack" file of that template. ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 
   it("throws an error if the details.flos array is not of the correct structure", () => {
     expect(() =>
       validateFlos(workflowName, { flos: [{ id: "a-1" }] }, { flos: [{ id: "a-1" }] })
-    ).toThrowError(
-      `The flos defined at the "${workflowName}/workflow.json -> details.flos" field contains invalid properties. Allowed fields are: "${validFloField.join(
-        ", "
-      )}". ${modifierScriptMsg}`
-    );
+    ).toThrowError();
   });
 
   it("passes if the flos data match between the flo and json file", () => {
     expect(() =>
       validateFlos(
         workflowName,
-        matchingFlosData.detailsFromFlopack,
-        matchingFlosData.detailsInJSON
+        {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        },
+        {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        }
       )
     ).not.toThrowError();
   });
@@ -234,22 +381,16 @@ describe("validateUseCases", () => {
   });
 
   it("throws an error if the useCases in json is not of type array", () => {
-    expect(() => validateUseCases(workflowName, "invalid-type")).toThrowError(
-      `The useCases field in "${workflowName}/workflow.json" has the wrong type. It should be an array of strings.`
-    );
+    expect(() => validateUseCases(workflowName, "invalid-type")).toThrowError();
   });
 
   it("throws an error if the useCases array is empty", () => {
-    expect(() => validateUseCases(workflowName, [])).toThrowError(
-      `The useCases field in "${workflowName}/workflow.json" can't be an empty array. It should be an array of strings. If not needed, you can delete it`
-    );
+    expect(() => validateUseCases(workflowName, [])).toThrowError();
   });
 
   it("throws an error if the values inside the useCases array are not valid", () => {
     // valid use cases === use cases defined in the useCases.json root file
-    expect(() => validateUseCases(workflowName, ["wrong-value"])).toThrowError(
-      `The use cases assigned to "${workflowName}/workflow.json" are not valid. Make sure the use cases are mentioned in the "useCases.json" file.`
-    );
+    expect(() => validateUseCases(workflowName, ["wrong-value"])).toThrowError();
   });
 
   it("throws an error if the useCases field contains elements not of type string", () => {
@@ -257,13 +398,169 @@ describe("validateUseCases", () => {
       validateUseCases(workflowName, [
         { title: "use case title", description: "use case description" }
       ])
-    ).toThrowError(
-      `The useCases field in "${workflowName}/workflow.json" contains wrong value for its elements. "useCases" should be an array of strings. Valid use cases exist in the "useCases.json" root file.`
-    );
+    ).toThrowError();
   });
 
   it("passes if valid use cases are provided", () => {
     const validUseCase = require("../../useCases.json").useCases[0].name;
     expect(() => validateUseCases(workflowName, [validUseCase])).not.toThrowError();
+  });
+});
+
+describe("validateScreenshots", () => {
+  it("passes if no flos exists", () => {
+    expect(() => validateScreenshots(
+      { flos: [] },
+      {
+        name: workflowName,
+        details: {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        }
+      }
+    )).not.toThrowError();
+  });
+
+  it("throws an error if there is no resources directory to house screenshots", () => {
+    function dirNotFoundError(message) {
+      this.message = message;
+      this.code = 'ENOENT';
+    }
+    jest.spyOn(fs, "readdirSync").mockImplementation(() => {
+      throw new dirNotFoundError("directory not found");
+    });
+    expect(() => validateScreenshots(
+      {
+        flos: [
+          {
+            id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+            name: "flo name 1",
+            type: "MAIN",
+          }
+        ]
+      },
+      {
+        name: workflowName,
+        details: {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        }
+      }
+    )).toThrowError();
+  });
+
+  it("throws an error when the screenshots and flos inside flopack don't match", () => {
+    const screenshots = ["screenshot-1.png", "screenshot-2.png"];
+    jest.spyOn(fs, "readdirSync").mockReturnValue(screenshots);
+    expect(() => validateScreenshots(
+      {
+        flos: [
+          {
+            id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+            name: "flo name 1",
+            type: "MAIN",
+          }
+        ]
+      },
+      {
+        name: workflowName,
+        details: {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        }
+      }
+    )).toThrowError();
+  });
+
+  it("throws an error when the screenshots and flos inside the json file don't match", () => {
+    const screenshots = ["screenshot-1.png", "screenshot-2.png"];
+    jest.spyOn(fs, "readdirSync").mockReturnValue(screenshots);
+    expect(() => validateScreenshots(
+      {
+        flos: [
+          {
+            id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+            name: "flo name 1",
+            type: "MAIN",
+          },
+          {
+            id: "883b80d8-a3ac-43cf-881b-e370efb55fc2",
+            name: "flo name 2",
+            type: "HELPER",
+          }
+        ]
+      },
+      {
+        name: workflowName,
+        details: {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            }
+          ]
+        }
+      }
+    )).toThrowError();
+  });
+
+  it("throws an error if a screenshot doesn't follow the URL pattern", () => {
+    jest.spyOn(fs, "readdirSync").mockReturnValue([
+      "PgHDqLQ3BjridgXTKhGSO3csfE.png",
+      "PlLWTpvWbaP96v54oUkKJE1bRH6JhV7jzBrutwiPr3I.png"
+    ]);
+    expect(() =>
+      validateScreenshots(
+        {
+          flos: [
+            {
+              id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+              name: "flo name 1",
+              type: "MAIN",
+            },
+            {
+              id: "883b80d8-a3ac-43cf-881b-e370efb55fc2",
+              name: "flo name 2",
+              type: "HELPER",
+            }
+          ]
+        },
+        {
+          name: workflowName,
+          details: {
+            flos: [
+              {
+                id: "edcc5dad-eef8-4651-b1db-1a6a85cf161e",
+                name: "flo name 1",
+                type: "MAIN",
+                screenshotURL: "https://d78vv2h34ll3s.cloudfront.net/static/catalog/workflows/${workflowName}/resources/3Bjg3MMU6mcJL8EB-PgHDqLQ3BjridgXTKhGSO3csfE.png"
+              },
+              {
+                id: "883b80d8-a3ac-43cf-881b-e370efb55fc2",
+                name: "flo name 2",
+                type: "HELPER",
+                screenshotURL: "screenshot-2"
+              }
+            ]
+          }
+        }
+      )
+    ).toThrowError();
   });
 });
